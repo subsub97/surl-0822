88f2b40412e667fa8a624f327cb3a598e4d62cea
Ho-Da-Dak <decoding22@gmail.com>
2025-09-01 22:58:13 +0900

    refactor: 이메일 인증 400 -> 409로 수정

--- src/main/java/com/megastudy/surlkdh/domain/member/exception/MemberErrorCode.java
+++ src/main/java/com/megastudy/surlkdh/domain/member/exception/MemberErrorCode.java
@@ -4,9 +4,9 @@ import com.megastudy.surlkdh.common.exception.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public enum MemberErrorCode implements ErrorCode {
-    NOT_FOUND("MEMBER.NOT_FOUND", "존재하지 않는 사용자입니다.", HttpStatus.BAD_REQUEST),
+    NOT_FOUND("MEMBER.NOT_FOUND", "존재하지 않는 사용자입니다.", HttpStatus.NOT_FOUND),
     INVALID_EMAIL("MEMBER.INVALID_EMAIL", "유효하지 않은 이메일 형식입니다.", HttpStatus.BAD_REQUEST),
-    DUPLICATE_EMAIL("MEMBER.DUPLICATE_EMAIL", "이미 사용 중인 이메일입니다.", HttpStatus.BAD_REQUEST),
+    DUPLICATE_EMAIL("MEMBER.DUPLICATE_EMAIL", "이미 사용 중인 이메일입니다.", HttpStatus.CONFLICT),
     UPDATE_FAILED("MEMBER.UPDATE_FAILED", "사용자 정보 업데이트에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
 
     private final String code;
